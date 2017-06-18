0e01059602a6832149b85683ff3186b9cba49494
darkwoolf10 <woolftreuser@yandex.ru>
2017-06-19 01:37:58 +0300

    delete about pages

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -39,7 +39,6 @@ AppAsset::register($this);
         'options' => ['class' => 'navbar-nav navbar-right'],
         'items' => [
             ['label' => 'Time of discounts', 'url' => ['/blog/']],
-            ['label' => 'About', 'url' => ['/site/about']],
             ['label' => 'Contact', 'url' => ['/site/contact']],
             Yii::$app->user->isGuest ? (
                 ['label' => 'Login', 'url' => ['/site/login']]
